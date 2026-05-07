a5f62f9828d4af7cecd00026fd8d5a3fb8acdaab
luki2508 <lgajzler06@gmail.com>
2026-05-07 18:33:30 +0200

    Fallback

---
+++ Assets/Fallback.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fallback : MonoBehaviour
+{
+    public string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(sceneName);
+
+    }
+}
