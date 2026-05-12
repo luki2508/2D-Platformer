f5e14dac4e7f139695cf32fa20c21cbbf6bcc713
luki2508 <lgajzler06@gmail.com>
2026-05-12 10:10:32 +0200

    DASH

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,6 +7,7 @@ public class PlayerMotor : MonoBehaviour
 {
     Vector2 direction;
     private bool canJump = true;
+    private bool canDash = true;
     private Rigidbody2D rigidbody2D;
     public float speed = 10;
     public float jumpForce = 10;
@@ -13,18 +15,27 @@ public class PlayerMotor : MonoBehaviour
     public float stoppingforce = 5;
     public float multijump;
     public float max_jumps = 2;
+    public float dashForce = 10;
+    private float dashTime;
+    public float dashDuration = 0.2f;
+
+
+  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         rigidbody2D=GetComponent<Rigidbody2D>();
 
     }
+
+  
     // Update is called once per frame
     private void FixedUpdate()
     {
         MovePlayer();
         HandleMaxSpeed();
         PlayerStopping();
+
 
     }
 
@@ -35,6 +46,12 @@ public class PlayerMotor : MonoBehaviour
 
     private void HandleMaxSpeed()
     {
+        if(dashTime > 0)
+        {
+            dashTime -= Time.fixedDeltaTime;
+            return;
+        }
+
         if (rigidbody2D.linearVelocityX >= maxspeed)
         {
             rigidbody2D.linearVelocityX = maxspeed;
@@ -77,7 +94,23 @@ public class PlayerMotor : MonoBehaviour
      private void OnCollisionEnter2D(Collision2D collision)
     {
         canJump = true;
+        canDash = true;
         multijump = max_jumps;
     }
+
+    private void OnDash()
+    {
+        if(canDash)
+        {
+            rigidbody2D.AddForce(Vector2.right * dashForce, ForceMode2D.Impulse);
+            dashTime = dashDuration;
+        }
+
+    }
+
+
+
+   
+
 
 }
